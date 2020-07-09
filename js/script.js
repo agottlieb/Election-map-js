8ca1fd4bc35e8dfc31a98e7eea2b3e668faa70b0
agottlieb <allison.m.gottlieb@gmail.com>
2020-07-09 13:46:22 -0400

    added election results for each candidate

--- js/script.js
+++ js/script.js
@@ -12,3 +12,12 @@ console.log (politician.name);
 
 var jane = createPolitician ("Jane Eyre");
 var edward = createPolitician ("Edward Rochester");
+
+jane.electionResults = [5,1,7,2,33,6,4,2,1,14,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,21,3,2,11,1,3,7,2];
+
+console.log (jane.electionResults);
+
+edward.electionResults = [4,2,4,4,22,3,3,1,2,15,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,17,3,1,2,11,2,3,1];
+
+
+console.log (edward.electionResults);
